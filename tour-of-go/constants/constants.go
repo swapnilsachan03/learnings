b779bd9ab6b9089361b9b42b63255797347feef3
Swapnil Sachan <swapnil.sachan2003@gmail.com>
2025-05-16 22:07:36 +0530

    complete packages, variables & functions

---
+++ tour-of-go/constants/constants.go
@@ -0,0 +1,42 @@
+package constants
+
+import "fmt"
+
+/*
+	Constants -->
+
+	* Constants are declared like variables, but with the const keyword
+	* Constants can be character, string, boolean, or numeric values
+	* Constants cannot be declared using the := syntax
+
+	Numeric Constants -->
+
+	* Numeric constants are high-precision values
+	* An untyped constant takes the type needed by its context
+	* Try printing needInt(Big) too (an int can store at maximum a 64-bit integer, and sometimes less)
+*/
+
+const Pi = 3.14
+
+const (
+	Big = 1 << 100
+	Small = Big >> 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+func Constants() {
+	const World = "world!"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+}
---
+++ tour-of-go/variables/variables.go
@@ -0,0 +1,37 @@
+package variables
+
+import "fmt"
+
+/*
+	Variables -->
+
+	* The var statement declares a list of variables; as in function argument lists, the type is last
+	* A var statement can be at package or function level. We see both in this example
+
+	Vairables with intializers -->
+
+	* A var declaration can include initializers, one per variable
+	* If an initializer is present, the type can be omitted; the variable will take the type of the initializer
+
+	Short variable declarations -->
+
+	* Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type
+	* Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available
+*/
+
+var c, python, java bool
+var x, y, z int = 1, 2, 3
+
+func Variables() {
+	var i int
+	var j, k, l = true, false, "no!"
+
+	lol := "lol xd"
+
+	fmt.Println(i, c, python, java)
+	fmt.Println(x, y, z)
+	fmt.Println(j, k, l)
+	fmt.Println(lol)
+
+	fmt.Println()
+}
