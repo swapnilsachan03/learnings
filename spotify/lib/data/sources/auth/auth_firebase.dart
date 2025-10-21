775f2b73f8f426fbcf254414d2f065fb92f6a71d
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2025-10-21 08:40:53 +0530

    flutter/spotify: store username when registering in firestore

--- spotify/lib/data/sources/auth/auth_firebase.dart
+++ spotify/lib/data/sources/auth/auth_firebase.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spotify/data/models/auth/create_user_req.dart';
@@ -12,10 +13,15 @@ class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
   Future<Either> signUp(CreateUserReq createUserReq) async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: createUserReq.email,
         password: createUserReq.password,
       );
+
+      FirebaseFirestore.instance.collection("users").add({
+        "name": createUserReq.fullName,
+        "email": data.user?.email,
+      });
 
       return Right("User registered successfully");
     } on FirebaseAuthException catch (e) {
