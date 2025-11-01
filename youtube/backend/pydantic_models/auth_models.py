caa4626c395b2d5826114b214f778cdd1df1b66a
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2025-11-01 14:39:38 +0530

    backend/youtube: implement user verification, login, token refresh api

--- youtube/backend/pydantic_models/auth_models.py
+++ youtube/backend/pydantic_models/auth_models.py
@@ -5,3 +5,13 @@ class SignupRequest(BaseModel):
     name: str
     email: str
     password: str
+
+
+class ConfirmSignupRequest(BaseModel):
+    email: str
+    otp: str
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
--- youtube/backend/routes/auth.py
+++ youtube/backend/routes/auth.py
@@ -1,10 +1,14 @@
 import boto3
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Cookie, Depends, HTTPException, Response
 from db.db import get_db
 from db.models.user import User
 from helpers.auth_helper import get_secret_hash
-from pydantic_models.auth_models import SignupRequest
+from pydantic_models.auth_models import (
+    ConfirmSignupRequest,
+    LoginRequest,
+    SignupRequest,
+)
 from secret_keys import SecretKeys
 from sqlalchemy.orm import Session
 
@@ -61,8 +65,119 @@ def signup_user(
         db.refresh(new_user)
 
         return {
-            "msg": "Signup successful. Please verify your email if required."
+            "message": "Signup successful. Please verify your email if required."
         }
+
+    except Exception as e:
+        raise HTTPException(400, f"Cognito signup exception: {e}")
+
+
+@router.post("/confirm-user")
+def confirm_signup(data: ConfirmSignupRequest):
+    try:
+        secret_hash = get_secret_hash(
+            data.email,
+            COGNITO_CLIENT_ID,
+            COGNITO_CLIENT_SECRET,
+        )
+
+        cognito_response = cognito_client.confirm_sign_up(
+            ClientId=COGNITO_CLIENT_ID,
+            Username=data.email,
+            ConfirmationCode=data.otp,
+            SecretHash=secret_hash,
+        )
+
+        return {"message": "Your email is now verified"}
+
+    except Exception as e:
+        raise HTTPException(400, f"Cognito signup exception: {e}")
+
+
+@router.post("/login")
+def login_user(data: LoginRequest, response: Response):
+    try:
+        secret_hash = get_secret_hash(
+            data.email,
+            COGNITO_CLIENT_ID,
+            COGNITO_CLIENT_SECRET,
+        )
+
+        cognito_response = cognito_client.initiate_auth(
+            ClientId=COGNITO_CLIENT_ID,
+            AuthFlow="USER_PASSWORD_AUTH",
+            AuthParameters={
+                "USERNAME": data.email,
+                "PASSWORD": data.password,
+                "SECRET_HASH": secret_hash,
+            },
+        )
+
+        auth_result = cognito_response.get("AuthenticationResult")
+
+        if not auth_result:
+            raise HTTPException(400, "Bad request")
+
+        access_token = auth_result.get("AccessToken")
+        refresh_token = auth_result.get("RefreshToken")
+
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+        )
+
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            secure=True,
+        )
+
+        return {"message": "User logged in successfully"}
+
+    except Exception as e:
+        raise HTTPException(400, f"Cognito signup exception: {e}")
+
+
+@router.post("/refresh")
+def refresh_token(
+    refresh_token: str = Cookie(None),
+    user_cognito_sub: str = Cookie(None),
+    response: Response = None,
+):
+    try:
+        secret_hash = get_secret_hash(
+            user_cognito_sub,
+            COGNITO_CLIENT_ID,
+            COGNITO_CLIENT_SECRET,
+        )
+
+        cognito_response = cognito_client.initiate_auth(
+            ClientId=COGNITO_CLIENT_ID,
+            AuthFlow="REFRESH_TOKEN_AUTH",
+            AuthParameters={
+                "REFRESH_TOKEN": refresh_token,
+                "SECRET_HASH": secret_hash,
+            },
+        )
+
+        auth_result = cognito_response.get("AuthenticationResult")
+
+        if not auth_result:
+            raise HTTPException(400, "Bad request")
+
+        access_token = auth_result.get("AccessToken")
+
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+        )
+
+        return {"message": "Access token is refreshed"}
 
     except Exception as e:
         raise HTTPException(400, f"Cognito signup exception: {e}")
