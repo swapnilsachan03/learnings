df7a697f4da5460111041521c8b0ef68311d2e8e
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2025-11-05 20:06:50 +0530

    backend/youtube: logic for spinning up ecs containers in consumer

--- youtube/consumer/main.py
+++ youtube/consumer/main.py
@@ -6,6 +6,8 @@ from secret_keys import SecretKeys
 secret_keys = SecretKeys()
 sqs_client = boto3.client("sqs", region_name=secret_keys.REGION_NAME)
 
+ecs_client = boto3.client("ecs", region_name=secret_keys.REGION_NAME)
+
 
 def poll_sqs():
     while True:
@@ -34,7 +36,39 @@ def poll_sqs():
                 bucket_name = s3_record["bucket"]["name"]
                 s3_key = s3_record["object"]["key"]
 
-                # spin up a docket container
+                response = ecs_client.run_task(
+                    cluster="arn:aws:ecs:ap-south-1:802084756196:cluster/learnings-youtube-transcoder-cluster",
+                    launchType="FARGATE",
+                    taskDefinition="arn:aws:ecs:ap-south-1:802084756196:task-definition/learnings-video-transcoder:2",
+                    overrides={
+                        "containerOverrides": [
+                            {
+                                "name": "learnings-video-transcoder",
+                                "environment": [
+                                    {"name": "S3_BUCKET", "value": bucket_name},
+                                    {"name": "S3_KEY", "value": s3_key},
+                                ],
+                            }
+                        ],
+                    },
+                    networkConfiguration={
+                        "awsvpcConfiguration": {
+                            "subnets": [
+                                "subnet-085bdd23cdcc087bf",
+                                "subnet-0de394d6ca12b3422",
+                                "subnet-0c282616bed6342a4",
+                            ],
+                            "assignPublicIp": "ENABLED",
+                            "securityGroups": ["sg-07bef0d162ac76b24"],
+                        }
+                    },
+                )
+
+                print(response)
+                sqs_client.delete_message(
+                    QueueUrl=secret_keys.AWS_SQS_RAW_VIDEO_PROCESSING,
+                    ReceiptHandle=message["ReceiptHandle"],
+                )
 
 
 poll_sqs()
